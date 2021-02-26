a9ca790d81f5537649ba3162ba53b2f77bb8bf7f
MenachemFuterfas <menachemfuterfas@gmail.com>
2021-02-25 22:28:13 -0500

    commit tree a472762f40d7c3d69b8c08a40c3a3aac0fa0dde6

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
 require 'pry'
-
+"hello this is a string."
+"hello this is also a string"
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end
